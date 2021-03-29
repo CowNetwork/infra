4ed273a2c37de882a6746fed7502d36ebd560a7a
Yannic Rieger <riegeryannic@gmail.com>
2021-03-29 18:45:02 +0200

    support subgroups in inventory script

--- inventory.py
+++ inventory.py
@@ -11,8 +11,8 @@ parser.add_argument('--host', type=str)
 args = parser.parse_args()
 
 out = subprocess.run(['terraform', '-chdir=tf', 'state', 'pull'], capture_output=True, text=True)
-
 state = json.loads(out.stdout)
+
 #print(state)
 #exit(1)
 
@@ -44,15 +44,26 @@ for instance in instances:
   inventory['_meta']['hostvars'][name] = {
     'ansible_host': attrs['ipv4_address']
   }
-  
+ 
+  inventory['all']['hosts'].append(name)
   groups = attrs['labels']['roles'].split('-')
-  for group in groups:
-    inventory['all']['hosts'].append(name)
-    
-    if group not in inventory:
-        inventory[group] = {'hosts':[]}
-    
-    inventory[group]['hosts'].append(name)
+
+  for entry in groups:
+    subgroups = entry.split('.')
+    for i in range(len(subgroups)):
+      group = subgroups[i]
+      if group not in inventory:
+        inventory[group] = {'hosts':[],'children':[]}
+      
+      inventory[group]['hosts'].append(name)
+      
+      # if last entry then we have no further subgroups
+      if i == len(subgroups) - 1:
+        break
+
+      nxt = subgroups[i + 1]
+      if nxt not in inventory[group]['children']:
+        inventory[group]['children'].append(nxt)
 
 
 if args.list:
