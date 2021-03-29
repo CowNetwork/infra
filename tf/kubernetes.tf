9570fa5343040008897348ee29a9a6ae32160898
Yannic Rieger <riegeryannic@gmail.com>
2021-03-29 18:45:37 +0200

    finialize cluster installation

--- tf/kubernetes.tf
+++ tf/kubernetes.tf
@@ -1,49 +1,23 @@
-resource "hcloud_network" "cluster-network" {
-  name = "cluster-network"
-  ip_range = "10.0.0.0/8"
-}
-
-resource "hcloud_network_subnet" "mars-network" {
-  network_id = hcloud_network.cluster-network.id
-  type = "cloud"
-  network_zone = "eu-central"
-  ip_range = "10.0.1.0/24"
-}
-
 resource "hcloud_server" "mars-masters" {
   count = 1 
-  name = "master${count.index + 1}.mars.cow.network"
+  name = "master${count.index + 1}-mars-cow-network"
   image = "debian-10"
   server_type = "cpx11"
   location = "fsn1"
   ssh_keys = ["management-key"]
   labels = {
-    roles = "kube_master-mars"
+    roles = "mars.kube_master"
   }
-  network {
-    network_id = hcloud_network.cluster-network.id
-    ip = "10.0.1.${count.index + 1}"
-  }
-  depends_on = [
-    hcloud_network_subnet.mars-network
-  ]
 }
 
 resource "hcloud_server" "mars-workers" {
   count = 4
-  name = "worker${count.index + 1}.mars.cow.network"
+  name = "worker${count.index + 1}-mars-cow-network"
   image = "debian-10"
   server_type = "cpx11"
   ssh_keys = ["management-key"]
   location = "fsn1"
   labels = {
-    roles = "kube_worker-mars"
+    roles = "mars.kube_worker"
   }
-  network {
-    network_id = hcloud_network.cluster-network.id
-    ip = "10.0.1.${count.index + 1 + length(hcloud_server.mars-masters)}"
-  }
-  depends_on = [
-    hcloud_network_subnet.mars-network
-  ]
 }
